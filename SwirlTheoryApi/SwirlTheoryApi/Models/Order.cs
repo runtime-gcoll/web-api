c525de34e4a8568e1c5e88a4e5c9da7f431f785c
Ted Regular <997633@students.gateshead.ac.uk>
2018-11-24 16:09:14 +0000

    Models
    Added Model classes from old project template

---
+++ SwirlTheoryApi/SwirlTheoryApi/Models/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApi.Models
+{
+    public class Order
+    {
+        [Key]
+        public int OrderId { get; set; }
+        [Required]
+        public DateTime OrderDate { get; set; }
+        [Required]
+        public OrderStatus OrderStatus { get; set; }
+    }
+}
